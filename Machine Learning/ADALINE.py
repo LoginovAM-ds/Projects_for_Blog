0cfa4a0d297fa7b385f47960321f5c51b567b51b
LoginovAM-ds <a.loginov.ds@gmail.com>
2023-11-27 21:04:53 +0200

    .

--- Machine Learning/ADALINE.py
+++ Machine Learning/ADALINE.py
@@ -1,2 +1,26 @@
 import numpy as np
 
+class AdalineGD(object):
+    def __init__(self, eta=0.01, n_iter=50):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for _ in range(self.n_iter):
+            net_input = self.net_input(X)
+            errors = y - net_input
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = 0.5 * (errors**2).sum()
+            self.cost_.append(cost)
+
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
